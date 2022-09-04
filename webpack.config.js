5e247856616f506406d0ca92e445d813f4c9f3d0
Ayoub <Ayoubrezaei79@gmail.com>
2022-09-04 23:43:14 +0430

    setup sass with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,34 @@ const htmlPlugin = new HtmlWebPackPlugin({
   template: "./src/index.html",
   filename: "./index.html",
 });
+
 module.exports = {
   mode: "development",
+  entry: path.resolve(__dirname, "./src/index.js"),
+  devtool: "eval-source-map",
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: ["babel-loader"],
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        test: /\.(s(a|c)ss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
   plugins: [htmlPlugin],
+  resolve: {
+    extensions: ["*", ".js", ".jsx"],
+  },
+  output: {
+    path: path.resolve(__dirname, "./public"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "./public"),
+    hot: true,
+  },
 };
