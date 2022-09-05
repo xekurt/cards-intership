e03686a0761c02e12834889694ff98b8c36bae94
Ayoub <Ayoubrezaei79@gmail.com>
2022-09-05 11:31:03 +0430

    initialize projects page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import ReactDOM from "react-dom";
 import React from "react";
+import "./style.scss";
+import Home from "./Pages/HomePage/Home";
+import ProjectsPage from "./Pages/ProjectsPage/ProjectsPage";
 
 const App = () => {
-  return <h1>This is my React app!</h1>;
+  return (
+    <>
+      <Home />
+      <ProjectsPage />
+    </>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("App"));
