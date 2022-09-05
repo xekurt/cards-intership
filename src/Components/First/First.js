9db379adc2f1c1d2662f31bef5ce8a48ac09f7ad
Ayoub <Ayoubrezaei79@gmail.com>
2022-09-05 14:14:04 +0430

    initialize first page

---
+++ src/Components/First/First.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Card from "./Card";
+import "./First.scss";
+const First = () => {
+  return (
+    <section id="first" className="container">
+      <Card />
+    </section>
+  );
+};
+
+export default First;
