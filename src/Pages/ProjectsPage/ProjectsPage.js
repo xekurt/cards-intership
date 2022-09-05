7879b9fe0ddbe28f419cb019bdc8193a55a9a692
Ayoub <Ayoubrezaei79@gmail.com>
2022-09-05 11:49:02 +0430

    initialize projects section

---
+++ src/Pages/ProjectsPage/ProjectsPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import First from "../../Components/First";
+import "./projects.scss";
+const ProjectsPage = () => {
+  return (
+    <>
+      <First />
+    </>
+  );
+};
+
+export default ProjectsPage;
