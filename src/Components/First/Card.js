bb9581d983d0d41106eb7214b5fb7364f6fe12a8
Ayoub <Ayoubrezaei79@gmail.com>
2022-09-06 16:26:46 +0430

    first release

--- src/Components/First/Card.js
+++ src/Components/First/Card.js
@@ -28,92 +28,91 @@ const Card = () => {
           CAMPING
         </button>
       </div>
+      <div className="flip">
+        <div className={`card ${active === "CAMPING" && "rotate"}`}>
+          <div className="front">
+            <div className="front__images">
+              <div className="img-wrapper-2">
+                <img src={sea} alt="treeTop" />
+              </div>
+              <div
+                className={`img-wrapper-5 disable ${
+                  active === "KAYAKING" && "reveal"
+                }`}
+              >
+                <img src={boat} alt="camp" />
+              </div>
 
-      <div className="back">
-        <div className="back__images">
-          <div
-            className={`img-wrapper-2 disable ${
-              active === "CAMPING" && "reveal"
-            }`}
-          >
-            <img src={treeTop} alt="treeTop" />
+              <div
+                className={`img-wrapper-4 disable ${
+                  active === "KAYAKING" && "reveal"
+                }`}
+              >
+                <img src={rock2} alt="rock" />
+              </div>
+            </div>
+            <div className="front__buble__container">
+              <div className="buble2"></div>
+              <p>KAYAKING</p>
+            </div>
+            <div className="front__data">
+              <h5>
+                <span className="dollar-sign">$</span>
+                39 / 4 <span>hrs</span>
+              </h5>
+              <p>per person</p>
+              <div className="location">
+                <img src={Location} alt="location" className="location__icon" />
+                <p>Drina , Serbia</p>
+              </div>
+              <button className="date-button-alt">Choose Date</button>
+            </div>
           </div>
-          <div
-            className={`img-wrapper-1 disable ${
-              active === "CAMPING" && "reveal"
-            }`}
-          >
-            <img src={camp} alt="camp" />
-          </div>
-          <div
-            className={`img-wrapper-3 disable ${
-              active === "CAMPING" && "reveal"
-            }`}
-          >
-            <img src={treeRight} alt="treeRight" />
-          </div>
-          <div
-            className={`img-wrapper-4 disable ${
-              active === "CAMPING" && "reveal"
-            }`}
-          >
-            <img src={rock} alt="rock" />
-          </div>
-        </div>
-        <div className="back__buble__container">
-          <div className="buble"></div>
-          <p>Camping</p>
-        </div>
-        <div className="back__data">
-          <h5>
-            <span className="dollar-sign">$</span>
-            29 / 8 <span>hrs</span>
-          </h5>
-          <p>per person</p>
-          <div className="location">
-            <img src={Location} alt="location" className="location__icon" />
-            <p>Tara , Serbia</p>
-          </div>
-          <button className="date-button">Choose Date</button>
-        </div>
-      </div>
+          <div className="back">
+            <div className="back__images">
+              <div className="img-wrapper-2">
+                <img src={treeTop} alt="treeTop" />
+              </div>
+              <div
+                className={`img-wrapper-1 disable ${
+                  active === "CAMPING" && "reveal"
+                }`}
+              >
+                <img src={camp} alt="camp" />
+              </div>
+              <div
+                className={`img-wrapper-3 disable ${
+                  active === "CAMPING" && "reveal"
+                }`}
+              >
+                <img src={treeRight} alt="treeRight" />
+              </div>
+              <div
+                className={`img-wrapper-4 disable ${
+                  active === "CAMPING" && "reveal"
+                }`}
+              >
+                <img src={rock} alt="rock" />
+              </div>
+            </div>
 
-      <div className="front">
-        <div className="front__images">
-          <div className="img-wrapper-2">
-            <img src={sea} alt="treeTop" />
+            <div className="back__buble__container">
+              <div className="buble"></div>
+              <p>Camping</p>
+            </div>
+            <div className="back__data">
+              <h5>
+                <span className="dollar-sign">$</span>
+                29 / 8 <span>hrs</span>
+              </h5>
+              <p>per person</p>
+              <div className="location">
+                <img src={Location} alt="location" className="location__icon" />
+                <p>Tara , Serbia</p>
+              </div>
+              <button className="date-button">Choose Date</button>
+            </div>
           </div>
-          <div
-            className={`img-wrapper-5 disable ${
-              active === "KAYAKING" && "reveal"
-            }`}
-          >
-            <img src={boat} alt="camp" />
-          </div>
-
-          <div
-            className={`img-wrapper-4 disable ${
-              active === "KAYAKING" && "reveal"
-            }`}
-          >
-            <img src={rock2} alt="rock" />
-          </div>
-        </div>
-        <div className="front__buble__container">
-          <div className="buble2"></div>
-          <p>Camping</p>
-        </div>
-        <div className="front__data">
-          <h5>
-            <span className="dollar-sign">$</span>
-            39 / 4 <span>hrs</span>
-          </h5>
-          <p>per person</p>
-          <div className="location">
-            <img src={Location} alt="location" className="location__icon" />
-            <p>Drina , Serbia</p>
-          </div>
-          <button className="date-button-alt">Choose Date</button>
         </div>
       </div>
     </div>
@@ -121,3 +120,58 @@ const Card = () => {
 };
 
 export default Card;
+{
+  /* <div className="back__images">
+<div
+  className={`img-wrapper-2 disable ${
+    active === "CAMPING" && "reveal"
+  }`}
+>
+  <img src={treeTop} alt="treeTop" />
+</div>
+<div
+  className={`img-wrapper-1 disable ${
+    active === "CAMPING" && "reveal"
+  }`}
+>
+  <img src={camp} alt="camp" />
+</div>
+<div
+  className={`img-wrapper-3 disable ${
+    active === "CAMPING" && "reveal"
+  }`}
+>
+  <img src={treeRight} alt="treeRight" />
+</div>
+<div
+  className={`img-wrapper-4 disable ${
+    active === "CAMPING" && "reveal"
+  }`}
+>
+  <img src={rock} alt="rock" />
+</div>
+</div> */
+}
+
+{
+  /* <div className="front__images">
+            <div className="img-wrapper-2">
+              <img src={sea} alt="treeTop" />
+            </div>
+            <div
+              className={`img-wrapper-5 disable ${
+                active === "KAYAKING" && "reveal"
+              }`}
+            >
+              <img src={boat} alt="camp" />
+            </div>
+
+            <div
+              className={`img-wrapper-4 disable ${
+                active === "KAYAKING" && "reveal"
+              }`}
+            >
+              <img src={rock2} alt="rock" />
+            </div>
+          </div> */
+}
